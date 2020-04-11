52190bd6ee85328ca44566298b872865a4379970
AleksandrMihasenko <aleksandr.mihasenko@gmail.com>
2020-04-11 16:42:57 +0300

    added js which calculates result

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
+const correctAnswers = ["B", "B", "B", "B"];
+const result = document.querySelector('.result');
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    
+    let score = 0;
+    const userAnswer = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
+
+    //check answers
+    userAnswer.forEach((answer, index) => {        
+        if (answer === correctAnswers[index]) {
+            score += 25;
+        }        
+    });
+
+    //show result
+    scrollTo(0, 0);
+    result.querySelector('span').textContent = `${score}%`;
+    result.classList.remove('result-none');
+
+    let output = 0;
+    const timer = setInterval(() => {
+        result.querySelector('span').textContent = `${output}%`;
+        if (output === score) {
+            clearInterval(timer);
+        } else {
+            output++;
+        }
+    }, 10);
+});
+
 
